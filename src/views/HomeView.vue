49723116cfaac3939ebac812d2675d94d4277854
kancilcakep99 <kancilcakep99@gmail.com>
2023-01-25 17:02:12 +0700

    feat: Add Mounted and Watch Property

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -10,7 +10,7 @@
         <!-- render if the directive's expression returns a false value -->
         <div v-else>
           <CardUnavailable
-            v-if="background == 2"
+            v-if="category == 2"
             :product="products"
             :increment="increment"
           />
@@ -19,8 +19,8 @@
       </div>
     </div>
     <!-- this tag use conditional rendering for background pattern each men, women, unavailable product -->
-    <div class="bg-pattern-men" v-if="background == 0"></div>
-    <div class="bg-pattern-women" v-else-if="background === 1"></div>
+    <div class="bg-pattern-men" v-if="category == 0"></div>
+    <div class="bg-pattern-women" v-else-if="category === 1"></div>
     <div class="bg=pattern-unavailable" v-else></div>
   </div>
 </template>
@@ -33,6 +33,7 @@ import CardSection from "@/components/CardSection.vue";
 import CardUnavailable from "@/components/CardUnavailable.vue";
 import LoaderSection from "@/components/LoaderSection.vue";
 // import library open source from dependency for request data through fakestoreapi
+import axios from "axios";
 
 export default {
   // name component for export file
@@ -47,8 +48,74 @@ export default {
     return {
       load: true,
       products: [],
-      background: 0,
+      category: 0,
+      number: 1,
     };
+  },
+  methods: {
+    set(data) {
+      this.products = data;
+    },
+    // method handlers used for increment event click button from child component
+    increment() {
+      if (this.number >= 20) {
+        this.number = 1;
+      } else {
+        this.number++;
+        console.log("data", this.number);
+      }
+    },
+  },
+  // mounted used for can access all Vue components, templates, data, events, and other global objects
+  mounted() {
+    console.log("data", this.number);
+    this.load = true;
+    // get API from fakestoreapi
+    axios
+      .get("https://fakestoreapi.com/products/" + this.number)
+      .then((response) => {
+        if (response.data.category == "men's clothing") {
+          this.category = 0;
+          console.log("men");
+        } else if (response.data.category == "women's clothing") {
+          this.category = 1;
+          console.log("women");
+        } else {
+          this.category = 2;
+          console.log("other");
+        }
+        console.log(response);
+        this.set(response.data);
+        this.load = false;
+      })
+      .catch((error) => console.log("Gagal : ", error));
+  },
+  // watch property used to do something in reaction to a particular data change
+  watch: {
+    // every time the number changes, this function runs
+    number() {
+      if (this.load == false) {
+        this.load = true;
+        // get API from fakestoreapi
+        axios
+          .get("https://fakestoreapi.com/products/" + this.number)
+          .then((response) => {
+            if (response.data.category == "men's clothing") {
+              this.category = 0;
+              console.log("men");
+            } else if (response.data.category == "women's clothing") {
+              this.category = 1;
+              console.log("women");
+            } else {
+              this.category = 2;
+              console.log("other");
+            }
+            this.set(response.data);
+            this.load = false;
+          })
+          .catch((error) => console.log("Gagal : ", error));
+      }
+    },
   },
 };
 </script>
